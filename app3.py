e16aabb4ae6cbd78a4e7e58d0fb05624314dc896
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-01-06 11:21:54 -0700

    Create app3.py

---
+++ app3.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from stmol import showmol
+import py3Dmol
+
+def render_mol(xyz):
+    xyzview = py3Dmol.view(width=400,height=400)
+    xyzview.addModel(xyz,'xyz')
+    xyzview.setStyle({'stick':{}})
+    xyzview.setBackgroundColor('white')#('0xeeeeee')
+    xyzview.zoomTo()
+    showmol(xyzview, height = 500,width=800)
+
+uploaded_files = st.sidebar.file_uploader("Choose xyz files", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    xyz = uploaded_file.getvalue().decode("utf-8")
+    render_mol(xyz)
