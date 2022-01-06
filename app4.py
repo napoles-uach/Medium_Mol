af5c9ddafd3a95c1aa86d4892f920ce9dccc7d40
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-01-05 21:00:50 -0700

    Create app4.py

---
+++ app4.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from stmol import showmol
+import py3Dmol
+
+from rdkit import Chem
+from rdkit.Chem import AllChem
+
+st.title('RDKit + Py3DMOL 😀')
+
+def makeblock(smi):
+    mol = Chem.MolFromSmiles(smi)
+    mol = Chem.AddHs(mol)
+    AllChem.EmbedMolecule(mol)
+    AllChem.MMFFOptimizeMolecule(mol, maxIters=200)
+    mblock = Chem.MolToMolBlock(mol)
+    return mblock
+
+def render_mol(xyz):
+    xyzview = py3Dmol.view()#(width=400,height=400)
+    xyzview.addModel(xyz,'mol')
+    xyzview.setStyle({'stick':{}})
+    xyzview.setBackgroundColor('white')
+    xyzview.zoomTo()
+    showmol(xyzview,height=500,width=500)
+
+compound_smiles=st.text_input('SMILES please','CC')
+blk=makeblock(compound_smiles)
+render_mol(blk)
